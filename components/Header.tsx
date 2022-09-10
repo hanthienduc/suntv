0f1f9f61b216d720c0bbe92fdd2b30f5b884019a
hanthienduc <hanthienduc.96@gmail.com>
2022-09-10 10:24:15 +0900

    initial commit add sample login

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
+      </div>
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
