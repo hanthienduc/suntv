f90f092ccf699817738c39139d8b4d0704e26b4f
hanthienduc <hanthienduc.96@gmail.com>
2022-09-10 11:07:15 +0900

    update base class

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,27 @@
-
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid/'
 function Header() {
   return (
     <header>
-      <div className="flex items-center space-x-2 md:">
+      <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
         />
-        <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
+
+        
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li >
+          <li className="headerLink">TV Shows</li >
+          <li className="headerLink">Movies</li >
+          <li className="headerLink">New & Popular</li >
+          <li className="headerLink">My List</li >
         </ul>
       </div>
-      <div></div>
+      <div>
+        <MagnifyingGlassIcon className='hidden  h-6 h-6 sm:inline' />
+      </div>
     </header>
   )
 }
