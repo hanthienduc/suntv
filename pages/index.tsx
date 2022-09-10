03f09cc03e16b40abaf982874153a576e2505f8f
hanthienduc <hanthienduc.96@gmail.com>
2022-09-10 13:47:40 +0900

    add style for home banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,29 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import { Movie } from '../typings'
 import requests from '../utils/request'
 
-const Home: NextPage = ({}) => {
+interface Props {
+  netflixOriginals: Movie[],
+  trendingNow: Movie[],
+  topRated: Movie[],
+  actionMovies: Movie[],
+  comedyMovies: Movie[],
+  horrorMovies: Movie[],
+  romanceMovies: Movie[],
+  documentaries: Movie[],
+}
+
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries, }: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 
     to-[#010511] lg:h-[140vh]">
@@ -14,7 +34,7 @@ const Home: NextPage = ({}) => {
       </Head>
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* Row */}
           {/* Row */}
