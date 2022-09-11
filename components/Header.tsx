d6c2819a6ac99a0c4ffd00aaed362d8baf363b0b
hanthienduc <hanthienduc.96@gmail.com>
2022-09-11 12:17:39 +0900

    finishing up styling the modal for displaying the video and add to my favorite feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
   }, [])
 
   return (
-    <header className={`${isScrolled ? 'bg=[#141414]' : ''}`}>
+    <header className={`${isScrolled ? 'bg-[#141414]' : ''}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
