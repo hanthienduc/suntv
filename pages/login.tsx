c3501330fb375a542643d8a852ea629224a57d22
hanthienduc <hanthienduc.96@gmail.com>
2022-09-17 14:37:01 +0900

    fix style for the navbar and edit the title for login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -26,7 +26,7 @@ function login() {
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
-        <title>Netflix</title>
+        <title>SunTV</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Image
